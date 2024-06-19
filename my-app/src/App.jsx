83ea10d80b9cff870decff6f3be47a6f96bc5597
Lamrabti Anasse <fuctura.20@gmail.com>
2024-06-19 01:05:07 +0100

    alot of commites

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import RouterController from "./components/RouterController";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <RouterController />
+    </div>
+  );
+}
+
+export default App;
